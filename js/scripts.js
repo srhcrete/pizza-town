da977f3915786f9671369fd4b69f8897f8aa4f88
Sarah Lindmar <srhcrete@gmail.com>
2017-08-25 09:35:15 -0700

    create pizza object from form data with values from 0-5

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
 
-
+function Pizza(size, sauce, crust, meat, veg) {
+  this.size = size;
+  this.sauce = sauce;
+  this.crust = crust;
+  this.meat = meat;
+  this.veg = veg;
+}
 
 
 
 
 $(document).ready(function() {
+  $('#pizza-form').submit(function(event) {
+    event.preventDefault();
 
+     var selectedSize =$('#select-size').val();
+     var selectedSauce = $('#select-sauce').val();
+     var selectedCrust = $('#select-crust').val();
+     var selectedMeat = $('#select-meat').val();
+     var selectedVeggie = $('#select-veg').val();
+     var newPizza = new Pizza (selectedSize, selectedSauce, selectedCrust, selectedMeat, selectedVeggie);
 
+     console.log(newPizza);
+
+   });
 
 });
