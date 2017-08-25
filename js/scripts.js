6109aeb58a006e60abf2a9f64e032f7568319dbf
Sarah Lindmar <srhcrete@gmail.com>
2017-08-25 16:42:54 -0700

    finish project

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 
-function Pizza(size, sauce, crust, meat, veg) {
+function Pizza(size, sauce, crust, toppings) {
   this.size = size;
   this.sauce = sauce;
   this.crust = crust;
@@ -14,27 +14,25 @@ Pizza.prototype.totalPrice = function() {
 var toppings = 0;
 function sumToppings(){
   $("input[type=checkbox]:checked").each(function(){
-    toppings += parseInt($(this).val());
+    toppings += parseFloat($(this).val());
   });
-  $("input[type=checkbox]:checked").prop("checked", false);
+
 }
 
 
 $(document).ready(function() {
   $('#pizza-form').submit(function(event) {
     event.preventDefault();
-    $('#show-pizza').remove();
-    console.log('#show-pizza');
+    $('#show-pizza').empty();
      var selectedSize = parseFloat($('#select-size').val());
      var selectedSauce = parseFloat($('#select-sauce').val());
      var selectedCrust = parseFloat($('#select-crust').val());
      sumToppings();
      var newPizza = new Pizza (selectedSize, selectedSauce, selectedCrust, toppings);
 
-console.log(newPizza.totalPrice());
+
 $('#show-pizza').append("$" + newPizza.totalPrice());
 console.log(newPizza);
-
 
    });
 
