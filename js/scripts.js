c7c70b8ae26ad45b750ed0dff7ae2d67be6bcb01
Sarah Lindmar <srhcrete@gmail.com>
2017-08-25 13:56:17 -0700

    remove array and add total price prototype for pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,5 @@
-var priceArr = [];
 
-// function addPrices(Pizza){
-//   for (var k in pizza) {
-//     priceArr.push(input[k]);
-//     console.log(priceArr);
-//   }
-// }
 
-var prices = {
-  0: "0",
-  1: "9.99",
-  2: "10.99",
-  3: "11.99",
-  4: "1.00",
-  5: "2.00",
-};
 
 function Pizza(size, sauce, crust, meat, veg) {
   this.size = size;
@@ -24,31 +9,32 @@ function Pizza(size, sauce, crust, meat, veg) {
   this.veg = veg;
   //this.price = new window.Price(this);
 }
-function addPrices (newPizza) {
-
-for (var k in newPizza) {
-     priceArr.push(newPizza[k]);
-   }
-    console.log(priceArr);
-}
-
+Pizza.prototype.totalPrice = function() {
+  return (this.size + this.sauce + this.crust + this.meat + this.veg).toFixed(2);
+};
 
 
 
 $(document).ready(function() {
   $('#pizza-form').submit(function(event) {
     event.preventDefault();
-
-     var selectedSize =$('#select-size').val();
-     var selectedSauce = $('#select-sauce').val();
-     var selectedCrust = $('#select-crust').val();
-     var selectedMeat = $('#select-meat').val();
-     var selectedVeggie = $('#select-veg').val();
+    $('#show-pizza').empty(newPizza);
+     var selectedSize = parseFloat($('#select-size').val());
+     var selectedSauce = parseFloat($('#select-sauce').val());
+     var selectedCrust = parseFloat($('#select-crust').val());
+     var selectedMeat = parseFloat($('#select-meat').val());
+     var selectedVeggie = parseFloat($('#select-veg').val());
      var newPizza = new Pizza (selectedSize, selectedSauce, selectedCrust, selectedMeat, selectedVeggie);
 
-     addPrices(newPizza);
+console.log(newPizza.totalPrice());
+$('#show-pizza').append(newPizza.totalPrice());
+console.log(newPizza);
 
-     console.log(newPizza);
+     //addPrices(newPizza);
+     //sum(newPizza);
+
+    //  console.log(newPizza);
+    //  console.log(newPizza.sum(this));
 
    });
 
