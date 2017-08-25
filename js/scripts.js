f8b332e60cb28a858f29fe6cd2d44a50789e2830
Sarah Lindmar <srhcrete@gmail.com>
2017-08-25 15:57:11 -0700

    add check boxes and function for adding multiple toppings to total price

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,37 @@ function Pizza(size, sauce, crust, meat, veg) {
   this.size = size;
   this.sauce = sauce;
   this.crust = crust;
-  this.meat = meat;
-  this.veg = veg;
-  //this.price = new window.Price(this);
+  this.toppings = toppings;
 }
+
 Pizza.prototype.totalPrice = function() {
-  return (this.size + this.sauce + this.crust + this.meat + this.veg).toFixed(2);
+  return (this.size + this.sauce + this.crust + this.toppings).toFixed(2);
 };
+
+var toppings = 0;
+function sumToppings(){
+  $("input[type=checkbox]:checked").each(function(){
+    toppings += parseInt($(this).val());
+  });
+  $("input[type=checkbox]:checked").prop("checked", false);
+}
+
 
 $(document).ready(function() {
   $('#pizza-form').submit(function(event) {
     event.preventDefault();
-    $('#show-pizza').empty(newPizza);
+    $('#show-pizza').remove();
+    console.log('#show-pizza');
      var selectedSize = parseFloat($('#select-size').val());
      var selectedSauce = parseFloat($('#select-sauce').val());
      var selectedCrust = parseFloat($('#select-crust').val());
-     var selectedMeat = parseFloat($('#select-meat').val());
-     var selectedVeggie = parseFloat($('#select-veg').val());
-     var newPizza = new Pizza (selectedSize, selectedSauce, selectedCrust, selectedMeat, selectedVeggie);
+     sumToppings();
+     var newPizza = new Pizza (selectedSize, selectedSauce, selectedCrust, toppings);
 
 console.log(newPizza.totalPrice());
 $('#show-pizza').append("$" + newPizza.totalPrice());
 console.log(newPizza);
 
-     //addPrices(newPizza);
-     //sum(newPizza);
-
-    //  console.log(newPizza);
-    //  console.log(newPizza.sum(this));
 
    });
 
